31ebde522464521d6cce8ee3a9fafe447506ff5c
Lionel_lo <longlionel@aol.com>
2022-03-24 15:08:31 +0100

    js firstName&lastName&mail

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,5 @@
+//function
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,10 +9,36 @@ function editNav() {
   }
 }
 
+//function régle regex for name and last name
+function isNameValid(name) {
+  const regexName = "[a-zA-Z]{3,}";
+  if (name.toLowerCase().match(regexName)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//function régle mail
+function isMailValid(mail) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (mail.match(mailformat)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const prenom = document.getElementById("first");
+const prenomHidden = document.querySelector("div.firstName div");
+const nom = document.getElementById("last");
+const nomHidden = document.querySelector("div.lastName div");
+const mail = document.getElementById("email");
+const emailHidden = document.querySelector("div.mail div");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,72 +48,41 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-//function régle regex for name and last name
-function isNameValid (name){
-  const regexName = '[a-zA-Z]{3,}';
-  if(name.toLowerCase().match(regexName)){
-    return true;
-  }else{
-    return false;
-  }
-}
 
-//function régle mail
-function isMailValid(mail){
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(mail.match(mailformat)){
-    return true
-  }else{
-    return false
-    }
-  }
 
 /*1er selection champ
 2e detecter l'envent quand user change value input
 3e cree fonction*/
 
 //pour le prenom
-const prenom = document.getElementById('first')
-prenom.addEventListener('input', event => {
-  isNameValid(prenom.value)
-  if (isNameValid(prenom.value)==false){
-    let hidden = document.querySelector('div.firstName div')
-    hidden.classList.remove('hidden');
-}else{
-  let hidden = document.querySelector('div.firstName div')
-    hidden.classList.add('hidden');
-}
-})
+prenom.addEventListener("input", (event) => {
+  isNameValid(prenom.value);
+  if (isNameValid(prenom.value) == false) {
+    prenomHidden.classList.remove("hidden");
+  } else {
+    prenomHidden.classList.add("hidden");
+  }
+});
 
-
-  //pour le nom
-const nom = document.getElementById('last')
-nom.addEventListener('input', event => {
-  isNameValid(nom.value)
-  if (isNameValid(nom.value)==false){
-    let hidden = document.querySelector('div.lastName div')
-    hidden.classList.remove('hidden');
-}else{
-  let hidden = document.querySelector('div.lastName div')
-    hidden.classList.add('hidden');
-}
-})
+//pour le nom
+nom.addEventListener("input", (event) => {
+  isNameValid(nom.value);
+  if (isNameValid(nom.value) == false) {
+    nomHidden.classList.remove("hidden");
+  } else {
+    nomHidden.classList.add("hidden");
+  }
+});
 
 //pour le mail
-const mail = document.getElementById('email')  
-mail.addEventListener('input', event => {
-  isMailValid(mail.value)
-  console.log(isMailValid(mail.value))
-  if (isMailValid(mail.value)==false){
-    let elt = document.querySelector('div.mail div')
-    elt.classList.remove('hidden');
-}else{
-  let elt = document.querySelector('div.mail div')
-    elt.classList.add('hidden');
-}
-})
-
-
+mail.addEventListener("input", (event) => {
+  isMailValid(mail.value);
+  if (isMailValid(mail.value) == false) {
+    emailHidden.classList.remove("hidden");
+  } else {
+    emailHidden.classList.add("hidden");
+  }
+});
 
 /*var birthdate =document.getElementById('birthdate')
 
